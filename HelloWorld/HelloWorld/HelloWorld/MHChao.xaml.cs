eab93fa5eb73f3fd29bef5e04670ffe712236b54
Hanhleuit <16520341@gm.uit.edu.vn>
2019-10-03 17:47:45 +0700

    BT 1

---
+++ HelloWorld/HelloWorld/HelloWorld/MHChao.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HelloWorld
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MHChao : ContentPage
+	{
+		public MHChao ()
+		{
+			InitializeComponent ();
+		}
+        private void cmdChao_Clicked(object sender,EventArgs e)
+        {
+            string HoTen = txtHoTen.Text;
+            DisplayAlert("Hello", "Hello: " + HoTen, "Thoat");
+        }
+	}
+}
---
+++ HelloWorld/HelloWorld/HelloWorld/MHTinhTong.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace HelloWorld
+{
+    public partial class MHTinhTong : ContentPage
+    {
+        public MHTinhTong()
+        {
+            InitializeComponent();
+        }
+        private void cmdTinhTong_Clicked(object sender, EventArgs e)
+        {
+            double soA, soB, tong;
+            soA = double.Parse(txtsoA.Text);
+            soB = double.Parse(txtsoB.Text);
+            tong = soA + soB;
+            txtSum.Text = tong.ToString();
+
+        }
+        private void cmdGoHom_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PopAsync();
+        }
+    }
+}
